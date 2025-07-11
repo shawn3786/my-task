c4e794a64ae51e1c9645ce7a2ee79b933c49cfe8
Muhammad Zeeshan <50868547+shawn3786@users.noreply.github.com>
2025-07-11 13:21:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import streamlit as st
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import os
+image_path = "welcome.jpg"
+img = Image.open(image_path).convert("RGBA")  # Add alpha channel
+draw = ImageDraw.Draw(img)
 
-st.set_page_config(page_title="Inventory App", layout="centered")
-welcome_image_path = "wellcome.jpg"
-img = Image.open(welcome_image_path)
-st.image(img, caption="Let's get started!", use_column_width=True)
+font = ImageFont.load_default()
+text = "Welcome to Inventory App"
+text_position = (30, 30)  # (x, y)
 
-
+draw.text(text_position, text, fill="white", font=font)
+st.image(img, use_column_width=True)
