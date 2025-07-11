2c01439a76f1b4b382041f0b65751bd623134ed7
Muhammad Zeeshan <50868547+shawn3786@users.noreply.github.com>
2025-07-11 13:31:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ image_path = "wellcome.jpg"
 img = Image.open(image_path).convert("RGBA")  # Add alpha channel
 draw = ImageDraw.Draw(img)
 
-font = ImageFont.truetype(size = 60)
+font = ImageFont.truetype("arial.ttf",size = 60)
 text = "Welcome to Inventory App"
 text_position = (130, 30)  # (x, y)
 
