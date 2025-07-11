a03531ba5bdb3ad62db8f195e8189ccaa84fd7a8
Muhammad Zeeshan <50868547+shawn3786@users.noreply.github.com>
2025-07-11 13:10:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from PIL import Image
 import os
 
 st.set_page_config(page_title="Inventory App", layout="centered")
-welcome_image_path = "/workspaces/my-task/Images/wellcome.jpg"
+welcome_image_path = "/wellcome.jpg"
 img = Image.open(welcome_image_path)
 st.image(img, caption="Let's get started!", use_column_width=True)
 
