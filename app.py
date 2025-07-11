601f3daa29694da5d70fc331d8b738a7ca168b5b
Muhammad Zeeshan <50868547+shawn3786@users.noreply.github.com>
2025-07-11 14:04:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,18 +9,18 @@ st.session_state.start_time = time.time()
 def go_to_menu():
     st.session_state.page = "menu"
 if st.season_state.page == "welcome":
-image_path = "wellcome.jpg"
-img = Image.open(image_path).convert("RGBA")  
-draw = ImageDraw.Draw(img)
-         
-font = ImageFont.load_default()
-text = "Welcome to Inventory App"
-text_position = (200, 30)  
-        
-draw.text(text_position, text, fill="black", font=font)
-st.image(img, use_column_width=True)
+    image_path = "wellcome.jpg"
+    img = Image.open(image_path).convert("RGBA")  
+    draw = ImageDraw.Draw(img)
+             
+    font = ImageFont.load_default()
+    text = "Welcome to Inventory App"
+    text_position = (200, 30)  
+            
+    draw.text(text_position, text, fill="black", font=font)
+    st.image(img, use_column_width=True)
 
-     if time.time() - st.session_state.start_time > 3:
+    if time.time() - st.session_state.start_time > 3:
         go_to_menu()
 elif st.session_state.page == "menu":
     st.title("📋 What would you like to do?")
