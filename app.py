3fad3031b03b38f1cddf36ef5fecd860c7d04944
Muhammad Zeeshan <50868547+shawn3786@users.noreply.github.com>
2025-07-11 13:42:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
 import streamlit as st
 from PIL import Image, ImageDraw, ImageFont
 import os
-image_path = "wellcome.jpg"
-img = Image.open(image_path).convert("RGBA")  
-draw = ImageDraw.Draw(img)
+import time
+def go_to_menu():
+    st.session_state.page = "menu"
+    image_path = "wellcome.jpg"
+    img = Image.open(image_path).convert("RGBA")  
+    draw = ImageDraw.Draw(img)
+    
+    font = ImageFont.load_default()
+    text = "Welcome to Inventory App"
+    text_position = (200, 30)  # (x, y)
+    
+    draw.text(text_position, text, fill="black", font=font)
+    st.image(img, use_column_width=True)
+    time.time() - st.session_state.start_time > 3:
+    go_to_menu()
 
-font = ImageFont.load_default()
-text = "Welcome to Inventory App"
-text_position = (200, 30)  # (x, y)
+    st.session_state.page == "menu":
+    st.title("📋 What would you like to do?")
+    col1, col2 = st.columns(2)
 
-draw.text(text_position, text, fill="black", font=font)
-st.image(img, use_column_width=True)
+    with col1:
+        st.button("📦 Start Inventory")
+        st.button("🚫 Add Finished Item")
+
+    with col2:
+        st.button("🛒 Make New Order")
+        st.button("⚠️ Check Low Stock")
