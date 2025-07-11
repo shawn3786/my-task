ffddead332963e46ea43937fd61cfe8adb005a2b
Muhammad Zeeshan <50868547+shawn3786@users.noreply.github.com>
2025-07-11 14:10:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ if st.session_state.page == "welcome":
     draw.text(text_position, text, fill="black", font=font)
     st.image(img, use_column_width=True)
 
-    if time.time() - st.session_state.start_time > 10:
+    if time.time() - st.session_state.start_time > 3:
         go_to_menu()
 elif st.session_state.page == "menu":
     st.title("📋 What would you like to do?")
