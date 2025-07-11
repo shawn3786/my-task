d8a5624e837095d673fee74a9a37f429bdf63d97
Muhammad Zeeshan <50868547+shawn3786@users.noreply.github.com>
2025-07-11 14:13:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 import streamlit as st
 from PIL import Image, ImageDraw, ImageFont
 import os
-import time
 
 
-st.session_state.page = "welcome"
-st.session_state.start_time = time.time()
-def go_to_menu():
-    st.session_state.page = "menu"
+
 if st.session_state.page == "welcome":
 
     image_path = "wellcome.jpg"
@@ -21,18 +17,18 @@ if st.session_state.page == "welcome":
     draw.text(text_position, text, fill="black", font=font)
     st.image(img, use_column_width=True)
 
-    if time.time() - st.session_state.start_time > 3:
-        go_to_menu()
-elif st.session_state.page == "menu":
-    st.title("📋 What would you like to do?")
+     if st.button("👉 Click to Continue"):
+        st.session_state.page = "menu"   
 
-    col1, col2 = st.columns(2)
-    with col1:
-        st.button("📦 Start Inventory")
-        st.button("🚫 Add Finished Item")
-    with col2:
-        st.button("🛒 Make New Order")
-        st.button("⚠️ Check Low Stock")
+        st.title("📋 What would you like to do?")
 
-
+        col1, col2 = st.columns(2)
+        with col1:
+            st.button("📦 Start Inventory")
+            st.button("🚫 Add Finished Item")
+        with col2:
+            st.button("🛒 Make New Order")
+            st.button("⚠️ Check Low Stock")
+    
+    
 
