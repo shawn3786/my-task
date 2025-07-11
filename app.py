739b7eafcd7ffcf0cff634cffa4554fca867aeaa
Muhammad Zeeshan <50868547+shawn3786@users.noreply.github.com>
2025-07-11 13:52:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,17 +13,19 @@ text_position = (200, 30)
         
 draw.text(text_position, text, fill="black", font=font)
 st.image(img, use_column_width=True)
-time.time() - st.session_state.start_time > 3
-go_to_menu()
-    
-st.session_state.page == "menu"
-st.title("📋 What would you like to do?")
-col1, col2 = st.columns(2)
-    
-with col1:
-    st.button("📦 Start Inventory")
-    st.button("🚫 Add Finished Item")
-    
-with col2:
-    st.button("🛒 Make New Order")
-    st.button("⚠️ Check Low Stock")
+
+def go_to_menu():
+    st.session_state.page = "menu"
+        
+if time.time() - st.session_state.start_time > 3:
+        go_to_menu()
+elif st.session_state.page == "menu":
+    st.title("📋 What would you like to do?")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.button("📦 Start Inventory")
+        st.button("🚫 Add Finished Item")
+    with col2:
+        st.button("🛒 Make New Order")
+        st.button("⚠️ Check Low Stock")
