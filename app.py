a4e541675138cb5d0e554c7ffa99f53bfde23abd
Muhammad Zeeshan <50868547+shawn3786@users.noreply.github.com>
2025-07-11 13:35:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import streamlit as st
 from PIL import Image, ImageDraw, ImageFont
 import os
 image_path = "wellcome.jpg"
-img = Image.open(image_path).convert("RGBA")  # Add alpha channel
+img = Image.open(image_path).convert("RGBA")  
 draw = ImageDraw.Draw(img)
 
-font = ImageFont.truetype("arial.ttf",size = 60)
+font = ImageFont.load_default()
 text = "Welcome to Inventory App"
-text_position = (130, 30)  # (x, y)
+text_position = (200, 30)  # (x, y)
 
 draw.text(text_position, text, fill="black", font=font)
 st.image(img, use_column_width=True)
